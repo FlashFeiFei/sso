88b734ec7fc4fb857db8dfff794f37bc3af7eea3
FlashFeiFei <51785816@qq.com>
2019-10-22 22:15:57 +0800

    update

--- build.sh
+++ build.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
+
 #基于gin框架，构建sso镜像，并推送到自己的dockerhub仓库
 sudo docker build --tag 51785816/sso:1.0 .
 
+
+#登录到dockerhub
+# sudo docker login
+
+##输入账号密码
+
+
+
+#推送到dockerhub
 sudo docker push 51785816/sso:1.0
